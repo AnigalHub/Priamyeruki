2eedc31d3022900bfd4915ecbae2cb718d0546d7
Ignatova Anna <anigal1997@gmail.com>
2020-09-09 16:53:41 +0300

    исправлены ошибки формы

--- src/components/LandingPage/feedback.vue
+++ src/components/LandingPage/feedback.vue
@@ -6,15 +6,15 @@
                 <form name="sentMessage" id="Form">
                     <b-row>
                         <b-col cols="4">
-                            <p class="text_night"> Ваше имя:</p>
+                            <label class="text_night" for="name">Ваше имя:</label>
                             <input type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
-                            <p class="text_night">Ваше телефон:</p>
+                            <label class="text_night" for="phone">Ваше телефон:</label>
                             <input type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
-                            <p class="text_night"> Ваш E-mail:</p>
+                            <label class="text_night" for="mail">Ваш E-mail:</label>
                             <input type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
                         </b-col>
                         <b-col>
-                            <p class="text_night"> Ваше сообщение:</p>
+                            <label class="text_night" for="message">Ваше сообщение:</label>
                             <textarea class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
                         </b-col>
                     </b-row>
