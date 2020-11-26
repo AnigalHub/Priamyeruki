21556474226a7da82a80b34476743a176c7e59d9
AnigalHub <anigal1997@gmail.com>
2020-11-26 16:42:03 +0300

    добавлен вывод карты из json

--- src/components/LandingPage/mymap.vue
+++ src/components/LandingPage/mymap.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="mymap">
-        <div class="feature map" id="map-wrap">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1337.5065661296035!2d37.90404039139417!3d55.68187746704363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab62f6d977c57%3A0xb6fd3dd42c79d783!2z0YPQuy4g0JjQvdC40YbQuNCw0YLQuNCy0L3QsNGPLCAzNNCQLCDQm9GO0LHQtdGA0YbRiywgTW9zY293IE9ibGFzdCwgMTQwMDAw!5e0!3m2!1sru!2sru!4v1595516566930!5m2!1sru!2sru" frameborder="0"  allowfullscreen="" aria-hidden="false" ></iframe>
+        <div class="feature map">
+            <iframe :src="map"></iframe>
         </div>
     </div>
 </template>
 <script>
+    import map from './../../../public/documents/address/map.json';
     export default {
-        name: "mymap"
+        name: "mymap",
+        data(){
+            return{
+                map:map,
+            }
+        }
     }
 </script>
 
