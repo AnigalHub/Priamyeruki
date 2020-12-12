e99595e2a40e3902fe31a3ac6939975f8ee887ee
AnigalHub <anigal1997@gmail.com>
2020-12-13 00:00:12 +0300

    добавлена привязка

--- src/components/LandingPage/feedback.vue
+++ src/components/LandingPage/feedback.vue
@@ -3,19 +3,19 @@
         <div id="background">
             <b-container>
                 <h2 class="h2_light">Обратная связь</h2>
-                <form name="sentMessage" id="Form">
+                <form name="sentMessage" id="Form"  @submit.prevent="SendFeedback">
                     <b-row>
                         <b-col cols="4">
                             <label class="text_night" for="name">Ваше имя:</label>
-                            <input v-model="feedbackData.userName" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
+                            <input v-model="feedback.userName" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
                             <label class="text_night" for="phone">Ваше телефон:</label>
-                            <input type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
+                            <input v-model="feedback.userPhone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
                             <label class="text_night" for="mail">Ваш E-mail:</label>
-                            <input type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
+                            <input v-model="feedback.userEmail" type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
                         </b-col>
                         <b-col>
                             <label class="text_night" for="message">Ваше сообщение:</label>
-                            <textarea class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
+                            <textarea v-model="feedback.userMessage" class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
                         </b-col>
                     </b-row>
                     <button type="submit" class="draw justify-content-end">Отправить сообщение</button>
