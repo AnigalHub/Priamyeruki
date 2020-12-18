31516ca14264b43ea0f0611fe61b3716d7a1f73f
AnigalHub <anigal1997@gmail.com>
2020-12-18 23:30:49 +0300

    добавлены стили при большой высоте экрана

--- src/components/company.vue
+++ src/components/company.vue
@@ -166,5 +166,20 @@
             height: 95px;
         }
     }
+    @media screen and (min-height: 992px){
+        svg{
+            margin-left: 0;
+            margin-right: auto;
+            float: left;
+            height: 130px;
+        }
+        h6{
+            text-align: left !important;
+           padding-top: 8% !important;
+        }
+        .flex-container > div {
+            width: 50%;
+        }
+    }
 
 </style>
