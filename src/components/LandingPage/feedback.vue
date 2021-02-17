457c20685e4798b50f48a09be9d5f8cb744d1e2d
AnigalHub <anigal1997@gmail.com>
2021-02-17 14:32:05 +0300

    исправлены именна данных (такие же как на сервере), добавлен url из файла

--- src/components/LandingPage/feedback.vue
+++ src/components/LandingPage/feedback.vue
@@ -7,15 +7,15 @@
                     <b-row>
                         <b-col cols="4">
                             <label class="text_night" for="name">Ваше имя:</label>
-                            <input v-model="feedback.userName" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
+                            <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
                             <label class="text_night" for="phone">Ваше телефон:</label>
-                            <input v-model="feedback.userPhone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
+                            <input v-model="feedback.phone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
                             <label class="text_night" for="mail">Ваш E-mail:</label>
-                            <input v-model="feedback.userEmail" type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
+                            <input v-model="feedback.email" type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
                         </b-col>
                         <b-col>
                             <label class="text_night" for="message">Ваше сообщение:</label>
-                            <textarea v-model="feedback.userMessage" class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
+                            <textarea v-model="feedback.message" class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
                         </b-col>
                     </b-row>
                     <button type="submit" class="draw justify-content-end">Отправить сообщение</button>
@@ -27,27 +27,28 @@
 
 <script>
     import axios from 'axios'
+    import cfg from './../../../public/documents/cfg.json'
     export default {
         name: "feedback",
         data(){
             return {
                 feedback:{
-                    userName: '',
-                    userPhone: '',
-                    userEmail: '',
-                    userMessage: '',
+                    name: '',
+                    phone: '',
+                    email: '',
+                    message: '',
                 }
             }
         },
         methods:{
             SendFeedback(){
-                const url = 'http://127.0.0.1:8000/SendFeedback';
+                const url = `${cfg.feedbackUrl}SendFeedback`;
                 axios.post(url, this.feedback)
                     .then(resp => {
                         alert(resp.data)
                     })
                     .catch(error => {
-                        console.log(error)
+                        alert(error)
                     })
                 //axios.get(url).then(resp => { alert(resp.data)}).catch(error => { console.log(error) })
             }
